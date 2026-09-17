8af86ed61dafeb6424535357a55ea19a59171b15
Miguel Borromeo <iggy47@gmail.com>
2026-09-17 01:29:34 -0500

    Changed the default backend server port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(process.cwd(), 'dist')));
 
 // TODO: Define API to act as a controller if the app needs to persist data
 
-const port = 3000;
+const port = 8000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
