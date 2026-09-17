a57b0471d1848f25dee49fb920cfe220475955f7
Miguel Borromeo <iggy47@gmail.com>
2026-09-16 20:25:24 -0500

    Added terminal slash to URL

--- server/server.js
+++ server/server.js
@@ -9,5 +9,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}/`);
 });
