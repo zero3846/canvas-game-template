7afa7a3ad595b0615749bb1c0df11bce056124cd
Miguel Borromeo <iggy47@gmail.com>
2026-09-16 19:36:13 -0500

    Moved code out of start() method to run directly on module load

--- main.js
+++ main.js
@@ -1,97 +1,70 @@
 import { isDebugEnabled, registerDebugWatch, updateDebugWatch } from "./debug.js";
 import { Direction, getCurrentDirection, initGame, onFrameRender, onFrameUpdate } from "./game.js";
 
-/**
- * The max frames-per-second to render the game at.
- * Set to undefined if there should be no set maximum.
- * @type {number|undefined}
- */
+/** @type {HTMLElement} */
+const overlay = document.querySelector("#debugOverlay");
+
+// Initialize the overlay's visibility
+overlay.style.visibility = isDebugEnabled() ? "visible" : "hidden";
+
+/** @type {HTMLCanvasElement} */
+const canvas = document.querySelector("#main");
+
+/** @type {CanvasRenderingContext2D} */
+const context = canvas.getContext("2d", {
+    alpha: false
+});
+
+// Adjust for high-density displays
+const dpr = window.devicePixelRatio || 1;
+canvas.width = canvas.clientWidth * dpr;
+canvas.height = canvas.clientHeight * dpr;
+context.scale(dpr, dpr);
+
+// The max frames-per-second to render the game at.
+// Set to 0 or less if there should be no set maximum.
 const fpsTarget = 5;
 
-/**
- * The time to wait (in milliseconds) until rendering the next frame.
- * This should be undefined if 'fpsTarget' is undefined.
- * @type {number|undefined}
- */
-const frameTimeWait = fpsTarget != null ? 1.0 / fpsTarget * 1000 : undefined;
+// The time to wait (in milliseconds) until rendering the next frame.
+const frameTimeWait = 1.0 / fpsTarget * 1000;
 
-/**
- * The measured frames-per-second.
- * @type {number}
- */
-let fps = 0;
-
-/**
- * The number of frames rendered since the last FPS measurement.
- * @type {number}
- */
+// The number of frames rendered since the last FPS measurement.
 let frameCount = 0;
 
-/**
- * The last time (in milliseconds) that a frame was rendered.
- * @type {number}
- */
+// The last time (in milliseconds) that a frame was rendered.
 let frameLastTime = 0;
 
-/**
- * The rendering context of the main canvas.
- * @type {CanvasRenderingContext2D}
- */
-let context;
+function animate(currentTime) {
+    const elapsedTime = currentTime - frameLastTime;
 
-// Start the game loop.
-start();
+    onFrameUpdate(currentTime);
 
-/**
- * Starts the rendering for the game. This method defines the rendering loop.
- * @param {HTMLCanvasElement} canvas The canvas to render the game onto.
- */
-function start() {
-    const overlay = document.querySelector("#debugOverlay");
-    overlay.style.visibility = isDebugEnabled() ? "visible" : "hidden";
-    
-    registerDebugWatch("fps");
-
-    const canvas = document.querySelector("#main");
-
-    context = canvas.getContext("2d", {
-        alpha: false
-    });
-
-    // Adjust for high-density displays
-    const dpr = window.devicePixelRatio || 1;
-    canvas.width = canvas.clientWidth * dpr;
-    canvas.height = canvas.clientHeight * dpr;
-    context.scale(dpr, dpr);
-
-    function animate(currentTime) {
-        const elapsedTime = currentTime - frameLastTime;
-
-        onFrameUpdate(currentTime);
-
-        if (frameTimeWait) {
-            if (elapsedTime >= frameTimeWait) {
-                frameLastTime = currentTime;
-                frameCount++;
-                onFrameRender(context);
-            }
-        } else {
+    if (fpsTarget <= 0) {
+        if (elapsedTime >= frameTimeWait) {
+            frameLastTime = currentTime;
             frameCount++;
             onFrameRender(context);
         }
-
-        requestAnimationFrame(animate);
+    } else {
+        frameCount++;
+        onFrameRender(context);
     }
+
+    // Continue the render loop
     requestAnimationFrame(animate);
-
-    // Refresh the FPS measurement
-    setInterval(() => {
-        fps = frameCount;
-        if (isDebugEnabled()) {
-            updateDebugWatch("fps", fps);
-        }
-        frameCount = 0;
-    }, 1000);
-
-    initGame();
 }
+
+// Start the render loop
+requestAnimationFrame(animate);
+
+// Refresh the FPS measurement
+registerDebugWatch("fps");
+setInterval(() => {
+    const fps = frameCount;
+    if (isDebugEnabled()) {
+        updateDebugWatch("fps", fps);
+    }
+    frameCount = 0;
+}, 1000);
+
+initGame();
